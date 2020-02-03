5bd3ec2b5ec05a5da86154b5e697b81e2a09ee4f
Hans Yelek <hnyelek@gmail.com>
2020-02-03 15:53:26 -0700

    Add JavaScript helper files

---
+++ src/js/constants.js
@@ -0,0 +1,6 @@
+export const PACKAGE_NAME = "@growth-tools/core";
+
+export const GROWTH_TOOLS_DOMAIN = "https://growthtools.com";
+
+// Domain where image assets for the npm package are stored.
+export const GOOGLE_STORAGE_DOMAIN = "https://storage.googleapis.com/growth-tools/core";
