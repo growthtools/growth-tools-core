6e95d43924a4852ce05d69a85d3541d7143a55c5
Hans Yelek <hnyelek@gmail.com>
2020-02-03 15:46:30 -0700

    Add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "umd"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.scss$/,
+        loader: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true }
+          }
+        ]
+      },
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".scss"],
+    alias: {
+      "assets": path.resolve(__dirname, "assets")
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin([
+      { from: "src/assets/fonts", to: "fonts" },
+    ]),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].[hash].css"
+    })
+  ],
+  externals: {
+    react: {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react",
+      umd: "react",
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs2: "react-dom",
+      commonjs: "react-dom",
+      amd: "react-dom",
+      umd: "react-dom",
+    },
+  }
+};
