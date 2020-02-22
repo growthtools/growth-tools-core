4b9dce51b1b67a67ec4219f30d868a9ca2c53d82
Hans Yelek <hnyelek@gmail.com>
2020-02-22 00:10:24 -0700

    *TEMPORARILY* add image assets
    This commit includes
    - local image assets, and
    - an update to the webpack configuration to process the images
    
    The image assets should be moved to Google Cloud storage where all the
    other images are located. This commit can be dropped once that's
    achieved.

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|jpg)$/,
+        loader: "url-loader",
+      },
+      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: "babel-loader"
