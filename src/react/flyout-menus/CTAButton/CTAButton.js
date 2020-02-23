7521970e94cfd179933cc9049c288ef605340fa8
Hans Yelek <hnyelek@gmail.com>
2020-02-23 05:02:52 -0700

    Flyout menu CTAButton: Update for CSS modules

--- src/react/flyout-menus/CTAButton/CTAButton.js
+++ src/react/flyout-menus/CTAButton/CTAButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {GOOGLE_STORAGE_DOMAIN} from "../../../js/constants";
-import "./styles.scss";
+import styles from "./styles.scss";
 
 const MENU_ICON = `${GOOGLE_STORAGE_DOMAIN}/icons/menu-green.svg`;
 
@@ -13,7 +13,7 @@ const CTAButton = ({
   label,
   href
 }) => (
-  <a className="flyout-cta-btn" href={href}>
+  <a className={styles["flyout-cta-btn"]} href={href}>
     <img src={MENU_ICON}alt="list icon"/>
     {label}
   </a>
