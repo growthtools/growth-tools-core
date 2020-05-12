e096ec80ff25275c48dd60add534f4517d3929fa
kacy hulme <kacyhulme@gmail.com>
2020-05-12 08:50:44 -0400

    add functionality to generate custom utm_source

--- src/js/utm_params.js
+++ src/js/utm_params.js
@@ -1,12 +1,23 @@
 function generateUTMparams(type, pathname) {
+  
+  function generateUTMsource() {
+    if (pathname == "generated-path") {
+      let workWithUsUTMsource = "add-custom-path-here"; //add custom UTM source here
+      return workWithUsUTMsource
+    } else {
+      let workWithUsUTMsource = pathname;
+      return workWithUsUTMsource
+    }
+  }
+  
+  let workWithUsUTMsource = generateUTMsource();
+  
   const workWithUsURL = "https://lp.growthtools.com/apply-course?utm_source=";
-  const workWithUsUTMsource = pathname;
   const workWithUsUTMcontent = "?utm_content=";
   const concat = workWithUsURL + workWithUsUTMsource + workWithUsUTMcontent;
-
   const workWithUsMobile = concat + "mobile-menu-link";
   const workWithUsDesktop = concat + "work-with-me-button";
-
+  
   if (type == "mobile") {
     return workWithUsMobile
   } else if (type == "desktop") {
